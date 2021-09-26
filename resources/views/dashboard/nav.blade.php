1620c17ad07e915b2614ec313b1c541fc04fd69b
ReOffSpark6 <eversondias10@live.com>
2021-09-26 16:28:15 -0300

    Dashboard, Changes Vertical Navbar

--- resources/views/dashboard/nav.blade.php
+++ resources/views/dashboard/nav.blade.php
@@ -1,9 +1,53 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky bg-primary">
-            <ul class="nav flex-column">
+    <div class="sidebar-sticky bg-primary ctl-dashnav">
+        <!-- New List -->
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a href="{{route('dashboard')}}"><img src="{{asset('img/icons/icon_dashboard.png')}}"> Dashboard </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#menu" data-toggle="collapse" class="ctl-droparrow"><img src="{{asset('img/icons/addIcon.png')}}"> Cadastro </a>
+            </li>
+            <div class="collapse list-group-level1" id="menu">
+                <a href="{{route('dashboard.cadastro.produto')}}" class="list-group-item">- Produto</a>
+                <a href="{{route('dashboard.cadastro.estoque')}}" class="list-group-item">- Estoque</a>
+                <a href="{{route('dashboard.cadastro.endereco')}}" class="list-group-item">- Endereço</a>
+                <a href="{{route('dashboard.cadastro.tipo_endereco.tipoEndereco') }}" class="list-group-item">- Tipo do Endereço</a>
+                <a href="#" class="list-group-item">- Lote</a>
+                <a href="#" class="list-group-item">- Localização</a>
+                <a href="#" class="list-group-item">- Sub-Especie</a>
+            </div>
+
+            <li class="nav-item">
+                <a href="#menu2" data-toggle="collapse" class="ctl-droparrow"><img src="{{asset('img/icons/icon_dashanalyst.png')}}"> Relatório </a>
+            </li>
+            <div class="collapse list-group-level1" id="menu2">
+                <a href="{{route('dashboard.cadastro.produto')}}" class="list-group-item">- Produto</a>
+                <a href="{{route('dashboard.cadastro.estoque')}}" class="list-group-item">- Estoque</a>
+                <a href="{{route('dashboard.cadastro.endereco')}}" class="list-group-item">- Endereço</a>
+                <a href="{{route('dashboard.cadastro.tipo_endereco.tipoEndereco') }}" class="list-group-item">- Tipo do Endereço</a>
+                <a href="#" class="list-group-item font-weight-bold bg-primary text-white">- Lote</a>
+                <a href="#" class="list-group-item font-weight-bold bg-primary text-white">- Localização</a>
+                <a href="#" class="list-group-item font-weight-bold bg-primary text-white">- Sub-Especie</a>
+            </div>
+
+            <li class="nav-item">
+                <a href="#menu3" data-toggle="collapse" class="ctl-droparrow"><img src="{{asset('img/icons/icon_dashconfig.png')}}"> Configuração </a>
+            </li>
+            <div class="collapse list-group-level1" id="menu3">
+                <a href="{{route('dashboard.configuracao.importacao')}}" class="list-group-item">- Importação</a>
+                <a href="{{route('dashboard.configuracao.exportacao')}}" class="list-group-item">- Exportação</a>
+                <a href="{{route('dashboard.configuracao.usuarios')}}" class="list-group-item">- Usuários</a>
+            </div>
+
+        </ul>
+        <!-- Old list -->
+        <!--
+        <ul class="nav flex-column">
               <li class="nav-item">
                 <a class="nav-link active bg-light text-primary font-weight-bold" href="{{ route('dashboard') }}">
-                  <img src="{{ asset('img/icons/home.png') }}" style="filter: white(100%);" width="20px" height="20px"></img>                   
+                  <img src="{{ asset('img/icons/icon_dashboard.png') }}" style="filter: white(100%);" width="20px" height="20px"></img>                   
 				    Home<span class="sr-only"></span>
                 </a>
               </li>
@@ -42,9 +86,10 @@
                 </div>
 			   </div> 
 			   
-            </ul>
-          </div>
-        </nav>
+        </ul>
+        -->
+    </div>
+</nav>
 		
 		<!-- List com sub-menu --
 		<div id="main-menu" class="list-group">
